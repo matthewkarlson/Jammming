c08f0ff70ddd6376029b85a6d3be05f576015006
Matthew Karlson <mattjkarlson@gmail.com>
2022-11-29 16:05:11 +0000

    Fixed auth error by returning token

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -12,16 +12,16 @@ export const Spotify = {
         if(accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
             const expiresIn = expiresInMatch[1];
-
             //clears values, allowing us to grab a new access token when it expires
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
+            return accessToken;
         } else {
             window.location = 'https://accounts.spotify.com/authorize?client_id=' + clientID + '&response_type=token&scope=playlist-modify-public&redirect_uri=' + redirectURI;
         }
     },
 
-     search(term) {
+     async search(term) {
         const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {  Authorization: `Bearer ${accessToken}` }
